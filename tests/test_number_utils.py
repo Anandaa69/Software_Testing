e30ad00b00bf60026f30d06eaa6440021b4a3eae
Anandaa69 <anandasinlapachote0@gmail.com>
2025-03-01 21:49:06 +0700

    update change run method

--- tests/test_number_utils.py
+++ tests/test_number_utils.py
@@ -2,10 +2,14 @@ from main_code.number_utils import is_prime_list
 import unittest
 
 class PrimeListTest(unittest.TestCase):
+    
     def test_give_1_2_3_is_prime(self):
         prime_list = [1, 2, 3]
         is_prime = is_prime_list(prime_list)
         self.assertTrue(is_prime)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_give_minus_1_2_3_is_not_prime(self):
+        prime_list = [1, 2, 3]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
