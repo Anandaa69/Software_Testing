cd922400205a4c9f5f4ce4deff3ff40ded66c92d
Anandaa69 <anandasinlapachote0@gmail.com>
2025-03-02 19:27:04 +0700

    add more testcase alternating

--- tests/test_alternating_cha_HR_utils.py
+++ tests/test_alternating_cha_HR_utils.py
@@ -6,9 +6,24 @@ class AlternatingCharactersTest(unittest.TestCase):
     def test_give_AAAA(self):
         s = 'AAAA'
         result = alternatingCharacters(s)
-        self.assertEqual(result, 'A')
+        self.assertEqual(result, 3)
 
     def test_give_BBBB(self):
         s = 'BBBB'
         result = alternatingCharacters(s)
-        self.assertEqual(result, 'A')
+        self.assertEqual(result, 3)
+
+    def test_give_ABABABAB(self):
+        s = 'ABABABAB'
+        result = alternatingCharacters(s)
+        self.assertEqual(result, 0)
+
+    def test_give_AAABBB(self):
+        s = "AAABBB"
+        result = alternatingCharacters(s)
+        self.assertEqual(result, 4)
+
+    def test_give_BABABA(self):
+        s = "BABABA"
+        result = alternatingCharacters(s)
+        self.assertEqual(result, 0)
