3c726b2fd45343f7fc7e39f19086a41ce8a2578b
Anandaa69 <anandasinlapachote0@gmail.com>
2025-03-02 21:37:15 +0700

    add more testcase in alternating_cha

--- tests/test_alternating_cha_HR_utils.py
+++ tests/test_alternating_cha_HR_utils.py
@@ -3,27 +3,52 @@ import unittest
 
 class AlternatingCharactersTest(unittest.TestCase):
 
-    def test_give_AAAA(self):
-        s = 'AAAA'
-        result = alternatingCharacters(s)
-        self.assertEqual(result, 3)
-
-    def test_give_BBBB(self):
-        s = 'BBBB'
-        result = alternatingCharacters(s)
-        self.assertEqual(result, 3)
-
-    def test_give_ABABABAB(self):
-        s = 'ABABABAB'
+    def test_single_char(self):
+        s = "A"
         result = alternatingCharacters(s)
         self.assertEqual(result, 0)
 
-    def test_give_AAABBB(self):
-        s = "AAABBB"
+    def test_long_alternating_string(self):
+        s = "ABABABABABABABAB"
+        result = alternatingCharacters(s)
+        self.assertEqual(result, 0)
+
+    def test_no_alternating(self):
+        s = "AAAABBBB"
+        result = alternatingCharacters(s)
+        self.assertEqual(result, 6)
+
+    def test_no_repeats(self):
+        s = "ABCDEF"
+        result = alternatingCharacters(s)
+        self.assertEqual(result, 0)
+
+    def test_even_length_alternating(self):
+        s = "ABABAB"
+        result = alternatingCharacters(s)
+        self.assertEqual(result, 0)
+
+    def test_single_change_needed(self):
+        s = "AABABAB"
+        result = alternatingCharacters(s)
+        self.assertEqual(result, 1)
+
+    def test_long_repeated_characters(self):
+        s = "AAAAAAAAAAAAAAAAAAAAAA"
+        result = alternatingCharacters(s)
+        self.assertEqual(result, 21)
+
+    def test_all_different_chars(self):
+        s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        result = alternatingCharacters(s)
+        self.assertEqual(result, 0)
+
+    def test_odd_length_alternating(self):
+        s = "ABABABA"
+        result = alternatingCharacters(s)
+        self.assertEqual(result, 0)
+
+    def test_repeated_chars_in_between(self):
+        s = "AABBBBA"
         result = alternatingCharacters(s)
         self.assertEqual(result, 4)
-
-    def test_give_BABABA(self):
-        s = "BABABA"
-        result = alternatingCharacters(s)
-        self.assertEqual(result, 0)
