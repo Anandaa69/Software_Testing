6f21a5fc5f727cfea87a89eeaf3c752f42a455a7
Anandaa69 <anandasinlapachote0@gmail.com>
2025-03-02 21:39:12 +0700

    add more testcase in caesar_cipher

--- tests/test_caesar_cipher_HR_utils.py
+++ tests/test_caesar_cipher_HR_utils.py
@@ -38,3 +38,58 @@ class CaesarCipherTest(unittest.TestCase):
         k = 2
         result = caesarCipher(s, k)
         self.assertEqual(result, "cBb")
+    
+    def test_empty_string(self):
+        s = ""
+        k = 5
+        result = caesarCipher(s, k)
+        self.assertEqual(result, "")
+
+    def test_no_shift(self):
+        s = "abcdef"
+        k = 0
+        result = caesarCipher(s, k)
+        self.assertEqual(result, "abcdef")
+
+    def test_large_shift_value(self):
+        s = "abc"
+        k = 26
+        result = caesarCipher(s, k)
+        self.assertEqual(result, "abc")
+
+    def test_wrap_around_upper_case(self):
+        s = "XYZ"
+        k = 5
+        result = caesarCipher(s, k)
+        self.assertEqual(result, "CDE")
+
+    def test_wrap_around_lower_case(self):
+        s = "xyz"
+        k = 5
+        result = caesarCipher(s, k)
+        self.assertEqual(result, "cde")
+
+    def test_mixed_case_and_special_characters(self):
+        s = "aB1!cD"
+        k = 3
+        result = caesarCipher(s, k)
+        self.assertEqual(result, "dE1!fG")
+
+    def test_large_shift_with_wrapping(self):
+        s = "abcXYZ"
+        k = 30  # จะเลื่อนมากกว่า 26 ครั้ง
+        result = caesarCipher(s, k)
+        self.assertEqual(result, "efgBCD")
+
+    def test_upper_case_and_numbers(self):
+        s = "HELLO 123"
+        k = 1
+        result = caesarCipher(s, k)
+        self.assertEqual(result, "IFMMP 123")
+
+    def test_mixed_string_with_punctuation(self):
+        s = "Hello, World!123"
+        k = 2
+        result = caesarCipher(s, k)
+        self.assertEqual(result, "Jgnnq, Yqtnf!123")
+        
